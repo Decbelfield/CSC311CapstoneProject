1c4c243d927e493d0659bcaa0910d94cccc9aaee
Decbelfield <Belfdj@farmingdale.edu>
2023-11-30 19:29:45 -0500

    Added MyLogger and UserSession code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.csc311capstoneproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.prefs;
 
 
     opens com.example.csc311capstoneproject to javafx.fxml;
